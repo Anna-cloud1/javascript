780644f5314cef1b420d4898599b281c95012718
Anna Chufarova <anyatrying@gmail.com>
2022-11-28 16:34:12 +0200

    push

---
+++ Tasks/less-21-dom/task-5/index.js
@@ -0,0 +1,8 @@
+// algo
+// 1 ищем класс
+// 2  устанавливаем заданный текст
+
+const setTitle = (text) =>
+  (document.querySelector(".title").textContent = text);
+
+  console.log(setTitle(`I'm done!`))
