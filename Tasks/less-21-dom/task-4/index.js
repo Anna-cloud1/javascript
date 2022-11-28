f5859bbf81c479e94fe588b4c0bb122a9a0c2ecb
Anna Chufarova <anyatrying@gmail.com>
2022-11-28 16:13:55 +0200

    push

---
+++ Tasks/less-21-dom/task-4/index.js
@@ -0,0 +1,30 @@
+// 1 создаем ф-цию
+// 2 ищем нужный текст эл
+// 3 возвращаем его
+
+export const getTitle = () => {
+  const element = document.querySelector('.title').textContent;
+  return element;
+};
+
+console.log(getTitle());
+
+export const getDescription = () => {
+  const element = document.querySelector('.about').innerText;
+  return element;
+};
+
+console.log(getDescription());
+
+export const getPlans = () => {
+  const element = document.querySelector('.plans').innerHTML;
+  return element;
+};
+console.log(getPlans());
+
+export const getGoals = () => {
+  const element = document.querySelector('.goal').outerHTML;
+  return element;
+};
+
+console.log(getGoals());
