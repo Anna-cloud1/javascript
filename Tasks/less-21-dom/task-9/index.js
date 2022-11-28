ea3a179587213a8ba0f58b7d44d20fc5883423a1
Anna Chufarova <anyatrying@gmail.com>
2022-11-28 19:11:09 +0200

    push

---
+++ Tasks/less-21-dom/task-9/index.js
@@ -0,0 +1,28 @@
+// 1 находим классы список и спешл
+// для каждого метода создаем переменные которые добавляют эл на страничку 
+// добавляем цифру
+// юзаем подходящий метод 
+
+
+const finishList = () => {
+  const listElem = document.querySelector('.list');
+  const special = document.querySelector('.special');
+
+  const listFirstElem = document.createElement('li');
+  listFirstElem.textContent = '1';
+  listElem.prepend(listFirstElem);
+
+  const listFourElem = document.createElement('li');
+  listFourElem.textContent = '4';
+  special.before(listFourElem);
+
+  const listSixElem = document.createElement('li');
+  listSixElem.textContent = '6';
+  special.after(listSixElem);
+
+  const listEightElem = document.createElement('li');
+  listEightElem.textContent = '8';
+  listElem.append(listEightElem);
+};
+
+finishList();
