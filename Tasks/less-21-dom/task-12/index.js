4213836f6738287365f0c9dee9ab1eb1b5c1f93d
Anna Chufarova <anyatrying@gmail.com>
2022-11-29 15:37:47 +0200

    ph

--- Tasks/less-21-dom/task-12/index.js
+++ Tasks/less-21-dom/task-12/index.js
@@ -1,8 +1,4 @@
-
 export const squaredNumbers = () => {
   const arr = Array.from(document.querySelectorAll('.number'));
-  arr.map(
-    (el) => (el.dataset.squaredNumber = el.dataset.number * el.dataset.number)
-  );
+  arr.map((el) => (el.dataset.squaredNumber = el.dataset * el.dataset));
 };
-
