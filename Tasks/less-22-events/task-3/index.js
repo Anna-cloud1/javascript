d39b1bef95a41844df6636e612ae94b068ab358f
Anna Chufarova <anyatrying@gmail.com>
2022-11-30 22:49:39 +0200

    push

--- Tasks/less-22-events/task-3/index.js
+++ Tasks/less-22-events/task-3/index.js
@@ -2,10 +2,10 @@ const taskStatus = document.querySelector('.task-status');
 
 const ifCheckedCheckbox = () => {
   if (taskStatus.checked === true) {
-    console.log('true');
+    console.log(true);
   }
   if (taskStatus.checked === false) {
-    console.log('false');
+    console.log(false);
   }
 };
 
