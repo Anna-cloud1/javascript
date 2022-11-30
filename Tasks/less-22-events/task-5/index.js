3484a80a8d19e5e3eb7ce70e62263f6644f81dae
Anna Chufarova <anyatrying@gmail.com>
2022-11-30 23:23:39 +0200

    fix

--- Tasks/less-22-events/task-5/index.js
+++ Tasks/less-22-events/task-5/index.js
@@ -2,6 +2,7 @@ const singleUseBtn = document.querySelector('.single-use-btn');
 
 const getClicked = () => {
   console.log('clicked');
+  singleUseBtn.removeEventListener('click', getClicked);
 };
 
-singleUseBtn.addEventListener('click', getClicked, { once: true });
+singleUseBtn.addEventListener('click', getClicked);
