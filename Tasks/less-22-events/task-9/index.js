7b92b00e55d0db973a315fd446794460256c8e70
Anna Chufarova <anyatrying@gmail.com>
2022-12-01 14:56:50 +0200

    fix

--- Tasks/less-22-events/task-9/index.js
+++ Tasks/less-22-events/task-9/index.js
@@ -1,7 +1,7 @@
 const buttons = Array.from(document.querySelectorAll('.pagination__page'));
 
-const getDataFromButtons = (event) => {
+const handleClick = (event) => {
   console.log(event.target.textContent);
 };
 
-buttons.map((el) => el.addEventListener('click', getDataFromButtons));
+buttons.map((el) => el.addEventListener('click', handleClick));
