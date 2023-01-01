c1a2b069262d49f44d3db9150ef43986c34c9579
Anna Chufarova <anyatrying@gmail.com>
2023-01-02 00:03:40 +0200

    push

--- Tasks/less-34/task-2/index.js
+++ Tasks/less-34/task-2/index.js
@@ -1,56 +1,90 @@
-const baseUrl = `https://63a8af9c100b7737b983b748.mockapi.io/tasks/users`;
+const baseUrl = `https://63a8af9c100b7737b983b748.mockapi.io/tasks/users?format=json`;
 
 function getUsersList() {
-  // put your code here
+  return fetch(baseUrl).then((response) => response.json());
 }
 
-export function getUserById(userId) {
-  // put your code here
+function getUserById(userId) {
+  return fetch(`${baseUrl}/${userId}`).then((response) => response.json());
 }
 
-export function createUser(userData) {
-  // put your code here
+function createUser(userData) {
+  return fetch(baseUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+    },
+    body: JSON.stringify(userData),
+  });
 }
 
-export function deleteUser(userId) {
-  // put your code here
+function deleteUser(userId) {
+  return fetch(`${baseUrl}/${userId}`, {
+    method: "DELETE",
+  });
 }
 
-export function updateUser(userId, userData) {
-  // put your code here
+function updateUser(userId, userData) {
+  return fetch(`${baseUrl}/${userId}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+    },
+    body: JSON.stringify(userData),
+  });
 }
 
 // examples
-getUsersList().then((users) => {
-  console.log(users); // array of the user objects [{'id':'1', 'firstName':'Grayce' ... }, {'id':'2', 'firstName':'Ara' ... }, ...]
-});
 
-getUserById('2').then((userData) => {
+let users = [
+  {
+    '1': {
+      id: "1",
+      email: "anya@email.com",
+      firstName: "Anna",
+      lastName: "Chufarova",
+      age: 21,
+    },
+    '2': {
+      id: "2",
+      email: "sasha@email.com",
+      firstName: "Sasha",
+      lastName: "Kozukhar",
+      age: 25,
+    },
+  },
+];
+
+// getUsersList(users).then((users) => {
+//   console.log(users); // array of the user objects [{'id':'1', 'firstName':'Grayce' ... }, {'id':'2', 'firstName':'Ara' ... }, ...]
+// });
+
+getUserById(users = '2').then((userData) => {
   console.log(userData); // {'id':'2', 'firstName':'Ara' ... }
 });
 
-const newUserData = {
-  email: 'cool@email.com',
-  firstName: 'Iron',
-  lastName: 'Man',
-  age: 42,
-};
+// const newUserData = {
+//   email: "cool@email.com",
+//   firstName: "Iron",
+//   lastName: "Man",
+//   age: 42,
+// };
 
-createUser(newUserData).then(() => {
-  console.log('User created');
-});
+// createUser(newUserData).then(() => {
+//   console.log("User created");
+// });
 
-const updatedUserData = {
-  email: 'new@email.com',
-  firstName: 'John',
-  lastName: 'Doe',
-  age: 17,
-};
+// const updatedUserData = {
+//   email: 'new@email.com',
+//   firstName: 'John',
+//   lastName: 'Doe',
+//   age: 17,
+// };
 
-updateUser('1', updatedUserData).then(() => {
-  console.log('User updated');
-});
+// updateUser('1', updatedUserData).then(() => {
+//   console.log('User updated');
+// });
 
-deleteUser('2').then(() => {
-  console.log('User updated');
-});
+// deleteUser('2').then(() => {
+//   console.log('User updated');
+// });
---
+++ Tasks/less-34/task-3/index.js
@@ -0,0 +1,39 @@
+// 1 найти все нужные дом эл
+// 2 сделать кнопку доступной для нажатия при всех заполненных полях
+// 3 отпр данные на сервер и получить с него данные с помощью алерта
+// 4 очистить поля ввода
+
+const url = `https://63a8af9c100b7737b983b748.mockapi.io/tasks/users`;
+
+const loginFormFields = document.querySelector('.login-form');
+const submitButton = document.querySelector('.submit-button');
+
+const ifFieldsValid = () => {
+  const validFields = loginFormFields.reportValidity();
+  if (!validFields) {
+    return;
+  }
+  submitButton.removeAttribute('disabled');
+};
+
+loginFormFields.addEventListener('input', ifFieldsValid);
+
+
+const serverResponse = () => {
+  const userData = Object.fromEntries(new FormData(loginFormFields));
+  return fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+    body: JSON.stringify(userData),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      alert(JSON.stringify(data));
+      loginFormFields.reset();
+      submitButton.setAttribute('disabled', true);
+    });
+};
+
+submitButton.addEventListener('input', serverResponse);
